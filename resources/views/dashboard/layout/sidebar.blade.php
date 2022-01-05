3a76b2739c645bf421d4434e9f4ad5664d862b1c
@danilo.cardoso <danilo.cardoso@vercan.com.br>
2022-01-05 14:53:39 -0300

    dashboard - adicionado module e limpeza

--- resources/views/dashboard/layout/sidebar.blade.php
+++ resources/views/dashboard/layout/sidebar.blade.php
@@ -1,9 +1,14 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
+
     <a href="#" class="brand-link">
         <span class="brand-text font-weight-light ml-5"><strong>Agência </strong>Turismo</span>
     </a>
+
+    <p class="brand-link">
+        <span class="brand-text font-weight-light ml-5">{{ auth::user()->name }}</span>
+    </p>
 
     <!-- Sidebar -->
     <div class="sidebar">
@@ -49,6 +54,13 @@
                     <a href="{{ route('home') }}" class="nav-link">
                         <i class="fas fa-arrow-right"></i>
                         <p>Site</p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="{{ route('logout') }}" class="nav-link">
+                        <i class="fas fa-sign-out-alt"></i>
+                        <p>Logout</p>
                     </a>
                 </li>
             </ul>
